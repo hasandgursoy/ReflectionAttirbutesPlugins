11f9105a43664c4e02cfe4dafc9caab7402cef5e
hasandgursoy <hasan.d.gursoy@gmail.com>
2023-07-02 23:52:04 +0300

    first commit

---
+++ ReflectedAttribute/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Person p1 = new Person { Name = "Hasan", Age = "22" };
+        Person p2 = new Person { Name = "Selina", Age = "90", ID = "923291" };
+
+        System.Console.WriteLine(Control.Check(p1));
+        System.Console.WriteLine(Control.Check(p2));
+
+    }
+}
+
+
+// Burada ki senaryo şu biz bir attribute yazalım ve bu attribute aşşağıda ki değerlerin boş geçilmesini engellesin.
+public class Person
+{
+    [Necessary]
+    public string Name;
+    [Necessary]
+    public string ID;
+    [Necessary]
+    public string Age;
+
+    public Person()
+    {
+    }
+
+    public Person(string name, string iD, string age)
+    {
+        Name = name;
+        ID = iD;
+        Age = age;
+    }
+    
+}
+
+// Birden fazla aynı attribute'u field'ın üzerine yazabilsin diye bunu yazıyoruz. False'u true'ya set edersek özellik açılacak.
+[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+public class NecessaryAttribute : Attribute
+{
+
+}
+
+public static class Control
+{
+
+    public static bool Check(Person person)
+    {
+        Type type = person.GetType();
+
+        foreach (var item in type.GetFields())
+        {
+            object[] attributes = item.GetCustomAttributes(typeof(NecessaryAttribute), true);
+
+            if (attributes.Length != 0)
+            {
+                object? value = item.GetValue(person);
+
+                if (value == null)
+                {
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+}
+
+public interface IPlugin
+{
+    string Name();
+    string Description();
+    void Run();
+}
---
+++ ReflectedPlugin/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        foreach (var item in Directory.GetFiles("bin/Debug/plugins", "*.dll"))
+        {
+            Assembly assembly = Assembly.LoadFrom(item);
+
+            foreach (var type in assembly.GetTypes())
+            {
+                var dd1  = type.FullName;
+                System.Console.WriteLine(dd1);
+                var dd = type.GetInterfaces();
+                System.Console.WriteLine(dd);
+
+                if (type.GetInterface("IPlugin") != null)
+                {
+                    // Activator.CreateInstance(T Type) içindeki type 'A göre bir tane nesne oluşturur.
+                    dynamic ins = Activator.CreateInstance(type);
+
+                    System.Console.WriteLine(ins.Name());
+                    System.Console.WriteLine(ins.Description());
+                    ins.Run();
+
+                }
+            }
+        }
+    }
+}
+
+public class HellloPlugin : IPlugin
+{
+    public string Description()
+    {
+        return "Bu bir eklenti description'ıdır.";
+    }
+
+    public string Name()
+    {
+        return "Selam verme eklentisi";
+    }
+
+    public void Run()
+    {
+        throw new NotImplementedException();
+    }
+}
